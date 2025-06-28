0a78028ae352723539bd8659311cb89e0ef055d1
陈烨庆 <2198626814@qq.com>
2025-06-28 23:15:51 +0800

    dev-ops(infrastructure): 更新开发环境配置和日志路径- 修改应用开发环境端口为 8080 - 更新数据库连接 URL - 调整 Docker Compose 配置- 添加 HelloController 用于测试- 更新 .gitignore 排除日志文件夹

---
+++ group-buy-market-cyq-trigger/src/main/java/cn/cyq/trigger/http/HelloController.java
@@ -0,0 +1,15 @@
+package cn.cyq.trigger.http;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World!";
+    }
+}
