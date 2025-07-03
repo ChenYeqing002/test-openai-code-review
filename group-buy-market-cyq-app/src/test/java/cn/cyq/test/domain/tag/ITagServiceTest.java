880384a680e369cbdbe166d86797ecbeff0c5cbf
陈烨庆 <2198626814@qq.com>
2025-07-03 11:56:30 +0800

    test: 添加 BitSet 测试用例
    - 在 ITagServiceTest 类中添加了 test_bigset 方法- 测试了 Redis BitSet 的基本操作，包括设置位、获取位和位运算
    - 验证了多个 BitSet 之间的与运算功能

--- group-buy-market-cyq-app/src/test/java/cn/cyq/test/domain/tag/ITagServiceTest.java
+++ group-buy-market-cyq-app/src/test/java/cn/cyq/test/domain/tag/ITagServiceTest.java
@@ -10,6 +10,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.annotation.Resource;
+import java.util.BitSet;
 
 @Slf4j
 @RunWith(SpringRunner.class)
@@ -21,6 +22,37 @@ public class ITagServiceTest {
 
     @Resource
     private IRedisService redisService;
+
+    @Test
+    public void test_bigset() {
+        // 获取BitSet
+        RBitSet mon = redisService.getBitSet("mon");
+        mon.set(3, true);
+        mon.set(5, true);
+        mon.set(7, true);
+
+        System.out.println("位操作前:" + mon.get(3));
+        System.out.println("位操作前:" + mon.get(5));
+        System.out.println("位操作前:" + mon.get(7));
+        System.out.println("位操作前:" + mon.get(8));
+
+        RBitSet thur = redisService.getBitSet("thur");
+        thur.set(3, true);
+        thur.set(5, true);
+        thur.set(8, true);
+
+        RBitSet wen = redisService.getBitSet("wen");
+        wen.set(3, true);
+        wen.set(4, true);
+        wen.set(6, true);
+
+        mon.and("thur", "wen");
+
+        System.out.println("位操作后:" + mon.get(3));
+        System.out.println("位操作后:" + mon.get(5));
+        System.out.println("位操作后:" + mon.get(7));
+        System.out.println("位操作后:" + mon.get(8));
+    }
 
     @Test
     public void test_tagJob() {
