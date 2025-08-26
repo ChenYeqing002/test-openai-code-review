ff3cf75087729bd54b33f8f3a6fda8442c272145
陈烨庆 <2198626814@qq.com>
2025-08-26 16:44:39 +0800

    feat: 第2-10节：责任链抽象模板设计-多例部分

---
+++ group-buy-market-cyq-app/src/test/java/cn/cyq/test/types/rule02/logic/RuleLogic201.java
@@ -0,0 +1,17 @@
+package cn.cyq.test.types.rule02.logic;
+
+import cn.cyq.test.types.rule02.factory.Rule02TradeRuleFactory;
+import cn.cyq.types.design.framework.link.model2.handler.ILogicHandler;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class RuleLogic201 implements ILogicHandler<String, Rule02TradeRuleFactory.DynamicContext, XxxResponse> {
+
+    @Override
+    public XxxResponse apply(String requestParameter, Rule02TradeRuleFactory.DynamicContext dynamicContext) throws Exception {
+        log.info("link model02 RuleLogic201");
+        return next(requestParameter, dynamicContext);
+    }
+}
